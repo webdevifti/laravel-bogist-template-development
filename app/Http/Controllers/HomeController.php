f1c839835bc822c8b7b18f294f755756b85aa048
webdevifti <eftialam@hotmail.com>
2022-02-07 21:20:56 +0600

    updating...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,17 +7,33 @@ use App\Models\Post;
 use App\Models\Social;
 use Exception;
 use Illuminate\Http\Request;
+use Phpfastcache\Helper\Psr16Adapter;
 
 class HomeController extends Controller
 {
     //
 
     public function index(){
+
+        // get instagram content 
+            $instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(),'be.ifti','beifti@FB#01', new Psr16Adapter('Files')); 
+            $instagram->login();
+            $instagram->saveSession();
+            $account = $instagram->getAccount('be.ifti');
+            $aacountMedias = $account->getMedias();
+            foreach($aacountMedias as $key => $accountMedia){
+                $images[$key] = str_replace('&amp;','&', $accountMedia->getimageHighResolutionUrl());
+                $path = $images[$key];
+                $imageName = $key.'.png';
+                $img = public_path('uploads/instagram/').$imageName;
+                file_put_contents($img, file_get_contents($path));
+            }
+        //end instagram content
         $getActiveSocialMedia = Social::where('status','=',1)->orderBy('created_at','DESC')->get();
         $getActivePosts = Post::where('status','=',1)->orderBy('created_at','DESC')->get();
         $featurePost = Post::inRandomOrder()->limit(3)->get();
         $getactivecategories = Category::where('status','=',1)->orderBy('created_at','DESC')->get();
-        return view('index', compact('getActivePosts','getActiveSocialMedia','featurePost','getactivecategories'));
+        return view('index', compact('getActivePosts','getActiveSocialMedia','featurePost','getactivecategories','images'));
     }
     public function show($slug){
         try{
